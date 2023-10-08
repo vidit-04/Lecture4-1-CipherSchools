6cc61e95a95b6fcfd4c8a1284f57957b2cd48de3
Vidit Gupta <117505027+vidit-04@users.noreply.github.com>
2023-10-08 22:51:32 +0530

    Add files via upload

---
+++ lecture_4I.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    int c,d;
+
+    cout<<"Input 4 integers a,b,c and d"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    // statement : a>b
+    cout<<(a>b && c>d);
+
+}
